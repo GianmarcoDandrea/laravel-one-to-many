d84c29969c15a0d9e189e5319dc0f727781ba3aa
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-26 16:45:15 +0100

    add: type select in create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -4,26 +4,42 @@
     <div class="container mt-5 w-50">
         <h2 class="text-center">Add New Project</h2>
 
-        @if ($errors->any())
-            <ul class="list-group">
-                @foreach ($errors->all() as $error)
-                    <li class="list-group-item list-group-item-danger">{{ $error }}</li>
-                @endforeach
-            </ul>
-        @endif
-
         <form class="mt-5" action="{{ route('admin.projects.store') }}" method="POST">
             @csrf
 
-            <div class="mb-3">
+            <div class="mb-3 has-validation">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
+                
+                @error('title')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
-            <div class="mb-3">
-                <label class="description-box" for="content" class="form-label">Description</label>
-                <textarea class="form-control" id="content" rows="3" name="description">{{ old('description') }}</textarea>
+            <div class="mb-3 has-validation">
+                <label class="description-box" for="description" class="form-label">Description</label>
+                <textarea class="form-control @error('description') is-invalid @enderror" id="description" rows="3" name="description">{{ old('description') }}</textarea>
+                
+                @error('description')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
+
+            <div class="mb-3 has-validation">
+			    <label for="category">Select type of your project:</label>
+			    <select class="form-select @error('type') is-invalid @enderror" name="category_id" id="type">
+			        <option @selected(!old('type_id')) value="">No category</option>
+
+			        @foreach ($types as $type)
+			            <option value="{{$type->id}}">{{$type->name}}</option>
+			        @endforeach
+
+			    </select>
+
+                @error('type_id')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+			</div>
 
             <button class="btn btn-success" type="submit">Save</button>
 
